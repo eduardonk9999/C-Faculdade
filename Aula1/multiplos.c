d60af3e778f41caeb8fea5c8a4de1ec7a6f7f185
Eduardo Silva <thiago.novak7@gmail.com>
2024-03-06 20:09:00 +0000

    multiplos

---
+++ Aula1/multiplos.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2, M;
+
+    printf("Insira o primeiro número\n");
+
+    scanf("%d\n",&n1);
+
+    printf("Insira o segundo número\n");
+
+    scanf("%d\n", &n2);
+    
+    M=n2*n1;
+
+    printf("O resultado é %d\n", M);
+}
